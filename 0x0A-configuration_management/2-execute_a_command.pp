c23b9369e9b2c627403a5e6fba7920a45804bf50
paisap <santiagoaldana58@gmail.com>
2020-03-23 20:55:21 +0000

    1 requirement

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Execute a proces
 exec { 'pkill killmenow':
-  path      => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command   => 'pkill killmenow',
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => 'pkill killmenow',
 }

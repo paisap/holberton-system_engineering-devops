59525bbb3572c2e1099b23c4805c1f06002e057d
paisap <santiagoaldana58@gmail.com>
2020-05-05 01:05:45 +0000

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,11 +7,12 @@ import sys
 
 if __name__ == "__main__":
     the_file = "{}.csv".format(sys.argv[1])
+    link = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
     link_2 = "https://jsonplaceholder.typicode.com/todos?userId=" + sys.argv[1]
-    x = requests.get("https://jsonplaceholder.typicode.com/users/2").json()
-    x = x["username"]
-    y = requests.get(link_2).json()
+    username = requests.get(link).json()
+    data = requests.get(link_2).json()
+    get_username = username["username"]
     with open(the_file, "w") as f:
-        for i in y:
+        for i in data:
             f.write('"{}","{}","{}","{}"\n'.format(i["userId"],
-                    x, i["completed"], i["title"]))
+                    get_username, i["completed"], i["title"]))

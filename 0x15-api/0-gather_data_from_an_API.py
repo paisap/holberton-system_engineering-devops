08f27cf9762b32f10e40b8751ffe8888ca78a0e2
paisap <santiagoaldana58@gmail.com>
2020-05-05 00:22:25 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Gather data from an API """
-
 import requests
 import json
 import sys
+
 if __name__ == "__main__":
     link = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
     link_2 = "https://jsonplaceholder.typicode.com/todos?userId=" + sys.argv[1]

e0fd4cdf85cbbf96473111a445dd7ed5896a0f41
paisap <santiagoaldana58@gmail.com>
2020-05-08 00:55:32 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import requests
 import json
+import requests
 
 
 def number_of_subscribers(subreddit):
+    """ get all subscribers """
     link = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
     try:
         x = requests.get(link, headers={'User-Agent': 'Santiago'}).json()
